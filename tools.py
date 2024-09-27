a7dd904f8052876a0d4bc14e32ce6a1e0a95d972
Raphpn61 <raphael.poinsignon@outlook.com>
2024-09-27 13:09:12 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,17 +1,1 @@
-def somme (a,b) :
-    return a+b
-
-def produit ( a, b ) :
-    return a*b
-
-def quotient (a, b ) :
-    return a/b
-
-def difference (a, b ) :
-    return a-b
-
-def quotient (a, b) :
-    return a//b
-
-def reste ( a,b ) :
-    return a%b
+print("camecourtsurleharicot")
