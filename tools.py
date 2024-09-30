6c55066bc4b7f1b0f90e9ca59da38dfdefbed19e
Raphpn61 <raphael.poinsignon@outlook.com>
2024-09-30 08:38:51 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,10 +1,10 @@
 def ask_for_the_init_number () : # la fonction renvoie un int 
-    init_nb = int (input ('Entrez le nombre de départ, qui doit être entier :'))
+    init_nb = str (input ('Entrez le nombre de départ, qui doit être entier :'))
     return init_nb
 
 def ask_for_the_init_base () : # Les bases sont des entiers
-    init_base = int (input ('Entrez la base de départ :'))
+    init_base = int (input ("Entrez la base de départ (2 pour le binaire, 10 pour le décimal, 16 pour l'hexadécimal) :"))
 
 def ask_for_the_target_base () : # La base est entière
-    target_base = int (input ('Entrez la base visée :'))
+    target_base = int (input ("Entrez la base visée (2 pour le binaire, 10 pour le décimal, 16 pour l'hexadécimal) :"))
 
