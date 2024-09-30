00017a8608167034e0e6313d66895a92959d85bd
Raphpn61 <raphael.poinsignon@outlook.com>
2024-09-30 09:32:01 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -8,3 +8,36 @@ def ask_for_the_init_base () : # Les bases sont des entiers
 def ask_for_the_target_base () : # La base est entière
     target_base = int (input ("Entrez la base visée (2 pour le binaire, 10 pour le décimal, 16 pour l'hexadécimal) :"))
 
+def bin_to_dec (x) :
+    pass
+#     v = [x]
+#     for c in v :
+
+#     return x
+
+def hex_to_dec () :
+    pass
+
+def dec_to_bin (c) :
+    v = []
+    while c // 2 > 0 :
+        v.append ( c%2 )
+        c = c // 2
+    return v
+print (dec_to_bin (11))
+def dec_to_hex () :
+    pass
+
+
+
+
+
+def bin_to_hex (b) :
+    b = bin_to_dec (b)
+    b = dec_to_bin (b)
+    return b
+
+def hex_to_bin (a) :
+    a = hex_to_dec(a)
+    a = dec_to_bin(a)
+    return a
