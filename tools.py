798bad97c069f9a0f3c72fe72a4203026f76f2c7
Raphpn61 <raphael.poinsignon@outlook.com>
2024-09-30 08:19:31 +0200

    Update tools.py
    On a les fonctions pour do_the_job

--- tools.py
+++ tools.py
@@ -1,1 +1,9 @@
-print("camecourtsurleharicot")
+def ask_for_the_init_number () : # la fonction renvoie un str donc pour comprendre le nombre je sais pas comment on fait
+    init_nb = input ('Entrez le nombre de départ :')
+    return init_nb
+
+def ask_for_the_init_base () : # Les bases sont des entiers
+    init_base = int (input ('Entrez la base de départ :'))
+
+def ask_for_the_target_base () : # La base est entière
+    target_base = int (input ('Entrez la base visée :'))
