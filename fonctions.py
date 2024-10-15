031b6e2193662893c9e7ad12af0de061b5f6de46
gabinoz <gabinozaneaux97@gmail.com>
2024-10-15 09:55:44 +0200

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -6,15 +6,46 @@
 #         start_number = start_number//2
 #     return restes
 
-def dec_to_hex(start_number):
-    base_hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
-    nb_hex_final = ""
-    while start_number > 0:
-        reste = start_number % 16
-        nb_hex = base_hex[reste]
-        nb_hex_final =  str(nb_hex) + nb_hex_final
-        start_number = start_number// 16
-    return nb_hex_final
+# def dec_to_hex(start_number):
+#     base_hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+#     nb_hex_final = ""
+#     while start_number > 0:
+#         reste = start_number % 16
+#         nb_hex = base_hex[reste]
+#         nb_hex_final =  str(nb_hex) + nb_hex_final
+#         start_number = start_number// 16
+#     return nb_hex_final
+
+# def bin_to_dec(start_number):
+#     cpt = 0
+#     nombre_en_decimal = 0
+#     start_number = str(start_number)
+#     for chiffres in start_number[::-1]:
+#         nombre_en_decimal += int(chiffres) * 2** cpt
+#         cpt += 1
+#     print(nombre_en_decimal)
+
+# bin_to_dec(1001)
+
+def hex_to_dec(start_number):
+     base_hex_nombres= [0,1,2,3,4,5,6,7,8,9,]
+     base_hex_chiffres = ["A","B","C","D","E","F"]
+     nombre_en_decimal = 0
+     cpt = 0
+     start_number = str(start_number)
+     for chiffres in start_number[::1]:
+         if chiffres in base_hex_nombres:
+             nombre_en_decimal += int(chiffres) * 16 ** cpt
+         else:
+             indice = base_hex_chiffres.index(chiffres)  # Obtenir l'indice
+             nombre_en_decimal += (10 + indice) * (16 ** cpt)
+         cpt += 1
+
+hex_to_dec("2A3F")
+         
+         
+        
+
 
 
 
