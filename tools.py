d633541fefb23287c9c9f0c0c97a58e68662e8d7
albertlgll <al.sauve.garde.8@gmail.com>
2024-11-14 16:31:30 +0100

    little change in tools

--- tools.py
+++ tools.py
@@ -46,13 +46,14 @@ def hex_to_dec(start_number):
     return nombre_en_decimal
 
 
-def dec_to_bin(start_number) :
+def dec_to_bin(start_number):
     restes = ""
     while start_number > 0:
         reste = start_number % 2    
-        restes = str(reste) + restes  #.append (reste)
-        start_number = start_number//2
+        restes = str(reste) + restes
+        start_number = start_number // 2
     return restes
+
 
 def dec_to_hex(start_number):
     base_hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
